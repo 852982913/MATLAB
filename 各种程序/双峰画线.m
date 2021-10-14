a8315ff3aedad14d12b2698eba8f25965550ea46
Mistletoe <63341466+852982913@users.noreply.github.com>
2021-10-14 20:20:08 +0800

    Delete 双峰画线.m

--- 各种程序/双峰画线.m
+++
@@ -1,7 +0,0 @@
-if (zong1-zong2)>0
-    line([heng1 heng1],[zong1 zong1-100]);
-    line([heng2 heng2],[zong2 zong2-100+zong1-zong2]);
-else
-    line([heng1 heng1],[zong1 zong1-100]);
-    line([heng2 heng2],[zong2 zong2-100]);
-end
