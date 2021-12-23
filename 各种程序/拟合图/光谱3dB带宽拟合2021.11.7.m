b1d82955fe240d4000e478e20e29b040cf2597de
Mistletoe <63341466+852982913@users.noreply.github.com>
2021-12-23 10:02:04 +0800

    Update 光谱3dB带宽拟合2021.11.7.m

--- 各种程序/拟合图/光谱3dB带宽拟合2021.11.7.m
+++ 各种程序/拟合图/光谱3dB带宽拟合2021.11.7.m
@@ -3,7 +3,7 @@ clear all
 clc
 format long;
 %———将数据文件名添加到元胞数组，对文件夹内所有.csv文件进行数据处理——
-path = 'C:\Users\wc852\Desktop\新建文件夹';           %添加数据文件夹绝对路径,工作区间要在同一个盘内
+path = 'D:\读研\实验数据\灌水论文\2021.12.22中心波长1560nm的谱宽变化\光谱';           %添加数据文件夹绝对路径,工作区间要在同一个盘内
 list = dir([path,'\*.csv']); %读取path路径下所有csv格式文件
 fileNames={list.name};      %将数据文件名添加到元胞数组
 disp(fileNames)             %显示变量 X 的值，而不打印变量名称
@@ -24,6 +24,18 @@ for i = 1:length(fileNames) %加载数据，放入矩阵数组
     txt_3db=[num2str(temh(end) - temh(1)),' nm'];%图上标注
     temha=(temh(1) + temh(end))/2
     text(temha,temz(end)-12,txt_3db,'fontsize',10);
+
+    zxbc_dbm=findpeaks(trace(:,2));%伪峰值
+    zxbc_dbm=sort(zxbc_dbm,'descend');%降序排列
+    zxbc_dbm1=zxbc_dbm(1);
+    [raw1,col1]=find(trace==zxbc_dbm1);%获取峰值点的坐标
+    zxbc_1=trace(raw1,1);
+    zxbc_1=zxbc_1(1)%出错时选择执行，直接执行也可以，但会降低程序效率
+    plot(zxbc_1,zxbc_dbm1,'.','color','r','MarkerSize',20);hold on;%峰值红点
+    txt=[num2str(zxbc_1),' nm'];%图上标注
+    nameda = 'λ =   ';
+    text(zxbc_1+10,zxbc_dbm1+3,txt,'fontsize',20);
+    text(zxbc_1,zxbc_dbm1+3,nameda,'fontsize',20);%符号λ
 % --------------- 调整图更美观 ------------------
     xlabel('Wavelength (nm)','fontname','微软雅黑','fontsize',14,'FontWeight','bold','Color','r')
     ylabel('Spectral intensity (dBm)','fontname','微软雅黑','fontsize',14,'FontWeight','bold','Color','r')
@@ -31,7 +43,7 @@ for i = 1:length(fileNames) %加载数据，放入矩阵数组
 % ------------------------存图------------------------
      mkdir csv_image;           %新建dat_image文件夹，如存在会警告，不影响程序
      filepath=pwd;              %保存当前工作目录
-     cd('C:\Users\wc852\Desktop\新建文件夹\csv_image');    %把当前工作目录切换到指定文件夹
+     cd('D:\读研\实验数据\灌水论文\2021.12.22中心波长1560nm的谱宽变化\光谱\csv_image');    %把当前工作目录切换到指定文件夹
      f = getframe(gcf);
     imwrite(f.cdata,[fileNames{i} '光谱拟合.png']);%将fileNames{i} 变量写入文件名
      cd(filepath); %返回工作目录
