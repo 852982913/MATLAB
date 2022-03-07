9c0cc5fb24d93365c3862808e0a1a0e29a3452d7
Mistletoe <63341466+852982913@users.noreply.github.com>
2022-03-07 19:47:37 +0800

    Update 重频差极低画分析图.m

--- 各种程序/分析图/重频差极低画分析图.m
+++ 各种程序/分析图/重频差极低画分析图.m
@@ -1,7 +1,7 @@
 % --------------------改进画图-------------------------
 clc
 % fengzhi(:,2)=fengzhi(:,2)*2; %只需执行一次
-shijian=csvread('C:\Users\酒仙桥科研巨擘专用\Desktop\123.csv');
+shijian=csvread('C:\Users\wc852\Desktop\123.csv');
 scatter(shijian(:,1),fengzhi(:,1),50,[0 0 0.9],'filled');%重频1  
 xlabel('Time(min)','fontname','微软雅黑','fontsize',14,'FontWeight','bold','Color','k'); 
 % ylabel('频率峰值(MHz)','fontname','微软雅黑','fontsize',14,'FontWeight','bold','Color','r'); 
@@ -24,17 +24,17 @@ h2.SizeData=50  %重频2的圆点大小
 
 txt1=[' Mean = ',num2str(Mean),' Hz'];%图上标注相关信息
 txt2=[' σ = ',num2str(std_cha),' Hz'];
-text(3,41.5727,txt1,'fontsize',15,'fontweight','bold','fontname','微软雅黑','Color','k')
-text(3,41.5726,txt2,'fontsize',15,'fontweight','bold','fontname','微软雅黑','Color','k')
-set(ax(2),'Ylim',[1054,1074]) %设置右侧y轴范围
-set(ax(2),'yTick',[1054:10:1074])%设置右侧y轴刻度
+text(1,68.994,txt1,'fontsize',15,'fontweight','bold','fontname','微软雅黑','Color','k')
+text(1,68.9945,txt2,'fontsize',15,'fontweight','bold','fontname','微软雅黑','Color','k')
+% set(ax(2),'Ylim',[1054,1074]) %设置右侧y轴范围
+% set(ax(2),'yTick',[1054:10:1074])%设置右侧y轴刻度
 
 % xlim([0 10])%x轴范围
 % set(ax(2),'xlim',[0,10])%x轴范围
 % set(gca,'YTick',[38:1:40]);%设置要显示坐标刻度
 % ylim([39.22 39.3])%y轴范围
-set(gca,'YTick',[41.5725,41.5735]);%设置要显示坐标刻度
-set(gca,'YTick',[41.5725:0.001:41.5735]);
+% set(gca,'YTick',[41.5725,41.5735]);%设置要显示坐标刻度
+% set(gca,'YTick',[41.5725:0.001:41.5735]);
 hold off;
 
 
